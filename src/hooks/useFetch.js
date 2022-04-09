3c56dbe9db0b03b0cd43e6e805c7649f9299603e
tceyhan <55tc155@gmail.com>
2022-04-09 17:46:45 +0300

    custom hook useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+
+import { useState, useEffect } from "react";
+
+
+function useFetch(url, initialdata = []) {
+  const [data, setData] = useState(initialdata);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          setError("Something went wrong!");
+          setLoading(false);
+        }else{
+        return response.json();
+        }
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+  return { data, loading, error };
+}
+export default useFetch;
